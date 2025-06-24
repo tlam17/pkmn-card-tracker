7b089ca307a8c641b98d3bd8f18a85d5d4cfaf72
tlam17 <94070835+tlam17@users.noreply.github.com>
2025-06-23 21:10:38 -0700

    Created service for extracting details from the JWT

---
+++ backend/src/main/java/com/tlam/backend/config/JWTService.java
@@ -0,0 +1,35 @@
+package com.tlam.backend.config;
+
+import java.security.Key;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+@Service
+public class JWTService {
+
+    @Value("${JWT_SECRET}")
+    private String SECRET_KEY;
+
+    public String extractUsername(String token) {
+        return null;
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
