a82763efe878546cff95f47a589f2f7dd64a7e3d
tlam17 <94070835+tlam17@users.noreply.github.com>
2025-06-25 20:56:36 -0700

    Initial commit

---
+++ backend/src/main/java/com/tlam/backend/test/TestController.java
@@ -0,0 +1,15 @@
+package com.tlam.backend.test;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+    @GetMapping
+    public ResponseEntity<String> sayHello() {
+        return ResponseEntity.ok("Hello, World!");
+    }
+}
