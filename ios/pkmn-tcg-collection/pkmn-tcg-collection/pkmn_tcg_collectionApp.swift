d125f91baae4d0f428bdfc068f630f940b6f981a
tlam17 <94070835+tlam17@users.noreply.github.com>
2025-06-27 22:00:06 -0700

    Initial commit

---
+++ ios/pkmn-tcg-collection/pkmn-tcg-collection/pkmn_tcg_collectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  pkmn_tcg_collectionApp.swift
+//  pkmn-tcg-collection
+//
+//  Created by Tyler Lam on 6/27/25.
+//
+
+import SwiftUI
+
+@main
+struct pkmn_tcg_collectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
