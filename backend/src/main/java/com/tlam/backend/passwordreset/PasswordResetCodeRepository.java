eca39aecaf027f43b5e44687b4d6d74a10de52bb
tlam17 <94070835+tlam17@users.noreply.github.com>
2025-07-07 19:22:46 -0700

    Added method for marking codes as used

--- backend/src/main/java/com/tlam/backend/passwordreset/PasswordResetCodeRepository.java
+++ backend/src/main/java/com/tlam/backend/passwordreset/PasswordResetCodeRepository.java
@@ -4,7 +4,14 @@ import java.time.LocalDateTime;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PasswordResetCodeRepository extends JpaRepository<PasswordResetCode, Long> {
     Optional<PasswordResetCode> findByEmailAndCodeAndIsUsedFalseAndExpiresAtAfter(String email, String code, LocalDateTime now);
+
+    @Modifying
+    @Query("UPDATE PasswordResetCode p SET p.isUsed = true WHERE p.email = :email")
+    void markAllCodesAsUsedForEmail(@Param("email") String email);
 }
