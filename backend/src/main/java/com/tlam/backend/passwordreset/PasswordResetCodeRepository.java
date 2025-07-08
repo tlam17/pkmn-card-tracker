c9ed0ad6863cf34e5f2b0775b2e13445ffa175b2
tlam17 <94070835+tlam17@users.noreply.github.com>
2025-07-07 23:01:51 -0700

    Added transactional annotation

--- backend/src/main/java/com/tlam/backend/passwordreset/PasswordResetCodeRepository.java
+++ backend/src/main/java/com/tlam/backend/passwordreset/PasswordResetCodeRepository.java
@@ -7,11 +7,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface PasswordResetCodeRepository extends JpaRepository<PasswordResetCode, Long> {
     Optional<PasswordResetCode> findByEmailAndCodeAndIsUsedFalseAndExpiresAtAfter(String email, String code, LocalDateTime now);
 
     @Modifying
+    @Transactional
     @Query("UPDATE PasswordResetCode p SET p.isUsed = true WHERE p.email = :email")
     void markAllCodesAsUsedForEmail(@Param("email") String email);
 }
