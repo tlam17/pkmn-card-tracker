f141f60e05022810036d364f9fa380549d45c1a3
tlam17 <94070835+tlam17@users.noreply.github.com>
2025-07-08 21:38:08 -0700

    Response model for general success responses

---
+++ ios/pokecollect/Core/Authentication/Models/SuccessResponse.swift
@@ -0,0 +1,21 @@
+//
+//  SuccessResponse.swift
+//  pokecollect
+//
+//  Created by Tyler Lam on 7/8/25.
+//
+
+
+//
+//  SuccessResponse.swift
+//  pokecollect
+//
+//  Created by Tyler Lam on 7/8/25.
+//
+
+import Foundation
+
+struct SuccessResponse: Codable {
+    let message: String
+    let timestamp: Int64
+}
