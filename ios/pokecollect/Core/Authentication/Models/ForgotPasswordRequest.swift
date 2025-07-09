6f12a450e814d822478f6ac68b4cced38071360a
tlam17 <94070835+tlam17@users.noreply.github.com>
2025-07-08 21:37:54 -0700

    Model for /forgot-password

---
+++ ios/pokecollect/Core/Authentication/Models/ForgotPasswordRequest.swift
@@ -0,0 +1,12 @@
+//
+//  ForgotPasswordRequest.swift
+//  pokecollect
+//
+//  Created by Tyler Lam on 7/8/25.
+//
+
+import Foundation
+
+struct ForgotPasswordRequest: Codable {
+    let email: String
+}
