3836ee6db7ca776d2da5016a89ea970c9bfeb096
tlam17 <94070835+tlam17@users.noreply.github.com>
2025-07-11 19:58:58 -0700

    Initial commit

---
+++ backend/src/main/java/com/tlam/backend/cardset/CardSetRepository.java
@@ -0,0 +1,7 @@
+package com.tlam.backend.cardset;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CardSetRepository extends JpaRepository<CardSet, String> {
+
+}
