378916089ede4bc1dfc1112075f2f5559712844b
tlam17 <94070835+tlam17@users.noreply.github.com>
2025-07-12 21:12:48 -0700

    Changed base url

--- backend/src/main/java/com/tlam/backend/pokemontcgapi/PokemonTCGConfigProperties.java
+++ backend/src/main/java/com/tlam/backend/pokemontcgapi/PokemonTCGConfigProperties.java
@@ -10,7 +10,7 @@ import lombok.Data;
 @ConfigurationProperties(prefix = "pokemon-tcg")
 public class PokemonTCGConfigProperties {
     private String apiKey;
-    private String baseUrl = "https://api.pokemontcg.io/v2/";
+    private String baseUrl = "https://api.pokemontcg.io/v2";
     private int connectTimeout = 30000;
     private int readTimeout = 30000;
     private int maxPageSize = 250;
