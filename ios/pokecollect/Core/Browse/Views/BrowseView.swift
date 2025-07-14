2b5109033988614c47d80a680883a89b4eaa41a9
tlam17 <94070835+tlam17@users.noreply.github.com>
2025-07-13 19:02:44 -0700

    Initial commit

--- ios/pokecollect/Core/Browse/Views/BrowseView.swift
+++ ios/pokecollect/Core/Browse/Views/BrowseView.swift
@@ -9,7 +9,34 @@ import SwiftUI
 
 struct BrowseView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            // Same background gradient as auth views
+            LinearGradient(
+                gradient: Gradient(colors: [
+                    Color.green.opacity(0.9),
+                    Color.teal.opacity(0.7),
+                    Color.mint.opacity(0.8)
+                ]),
+                startPoint: .topLeading,
+                endPoint: .bottomTrailing
+            )
+            .ignoresSafeArea()
+            
+            VStack(spacing: 20) {
+                Image(systemName: "rectangle.stack.fill")
+                    .font(.system(size: 60))
+                    .foregroundColor(.white)
+                
+                Text("Browse Sets & Cards")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                
+                Text("Coming soon...")
+                    .font(.subheadline)
+                    .foregroundColor(.white.opacity(0.8))
+            }
+        }
     }
 }
 
