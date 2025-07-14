b1b98b1b9341c025ca973574f47f54c22d66c04b
tlam17 <94070835+tlam17@users.noreply.github.com>
2025-07-13 18:17:50 -0700

    Added new method

--- backend/src/main/java/com/tlam/backend/cardset/CardSetRepository.java
+++ backend/src/main/java/com/tlam/backend/cardset/CardSetRepository.java
@@ -1,7 +1,9 @@
 package com.tlam.backend.cardset;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CardSetRepository extends JpaRepository<CardSet, String> {
-
+    List<CardSet> findBySeriesOrderByReleaseDateDesc(String series);
 }
