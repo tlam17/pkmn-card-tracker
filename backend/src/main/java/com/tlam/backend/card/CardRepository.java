ff6062059f376eced21b6a1fac45cf5d6acd1564
tlam17 <94070835+tlam17@users.noreply.github.com>
2025-07-16 16:14:45 -0700

    Initial commit

---
+++ backend/src/main/java/com/tlam/backend/card/CardRepository.java
@@ -0,0 +1,9 @@
+package com.tlam.backend.card;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CardRepository extends JpaRepository<Card, String> {
+    List<Card> findBySetIdOrderByNumberAsc(String setId);
+}
