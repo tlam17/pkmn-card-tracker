31d6b2e72a78990cb23ffbbb4cc9b428a365363b
tlam17 <94070835+tlam17@users.noreply.github.com>
2025-07-16 16:07:46 -0700

    Created Card entity

---
+++ backend/src/main/java/com/tlam/backend/card/Card.java
@@ -0,0 +1,58 @@
+package com.tlam.backend.card;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "cards")
+public class Card {
+    @Id
+    @NotBlank
+    private String id;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String name;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String number;
+
+    @NotBlank
+    @Column(nullable = false, name = "set_id")
+    private String setId;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String rarity;
+
+    @Column(name = "small_image_url")
+    private String smallImageUrl;;
+
+    @Column(name = "large_image_url")
+    private String largeImageUrl;
+
+    @CreationTimestamp
+    @Column(updatable = false, name = "created_at")
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
