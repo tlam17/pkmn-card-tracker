ffe73e1b6f663f16932ce776f357febdde406efc
tlam17 <94070835+tlam17@users.noreply.github.com>
2025-07-30 20:11:13 -0700

    Removed unused import

--- backend/src/main/java/com/tlam/backend/cardset/CardSet.java
+++ backend/src/main/java/com/tlam/backend/cardset/CardSet.java
@@ -14,7 +14,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
